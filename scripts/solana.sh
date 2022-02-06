01810013c2e836deba6b61712ad0084f6399156a
juanito87 <juancruz.f87@gmail.com>
2022-02-06 17:35:31 -0300

    Updating repo

--- scripts/solana.sh
+++ scripts/solana.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 tmux neww -n:deploy
 tmux split-window -h
+tmux send 'a29p' ENTER
 tmux split-window -v
+tmux send 'a29p' ENTER
 tmux split-window -v
+tmux send 'a29p' ENTER
 tmux select-layout main-vertical
