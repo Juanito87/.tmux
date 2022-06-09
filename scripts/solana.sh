a0fa654b6c5b357b9847b5a614dca76fca5de0aa
juanito87 <juancruz.f87@gmail.com>
2022-06-09 08:35:34 -0300

    Updating deployment window script

--- scripts/solana.sh
+++ scripts/solana.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 tmux neww -n:deploy
 tmux split-window -h
-tmux send 'a29p' ENTER
+tmux send 'a29' ENTER
+tmux send 'cd rpcpool' ENTER
 tmux split-window -v
-tmux send 'a29p' ENTER
+tmux send 'a29' ENTER
+tmux send 'cd rpcpool' ENTER
 tmux split-window -v
-tmux send 'a29p' ENTER
+tmux send 'a29' ENTER
+tmux send 'cd rpcpool' ENTER
 tmux select-layout main-vertical
